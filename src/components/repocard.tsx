bd897dcddaad7bfc6ca9046c7aa48aa13f404597
aashhshah <aashhshah1997@gmail.com>
2023-03-18 20:21:43 +0530

    Added card to show repo info

--- src/components/repocard.tsx
+++ src/components/repocard.tsx
@@ -1,6 +1,9 @@
-import { Avatar, Card, CardContent } from "@mui/material";
-
-
+import { lexend } from "@/theme";
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import { Box, Card, Divider, Typography } from "@mui/material";
+import Button from '@mui/material/Button';
+import PopupState, { bindHover, bindPopover } from 'material-ui-popup-state';
+import HoverPopover from "material-ui-popup-state/HoverPopover";
 export interface Props {
   avtarurl: string | null
   repo_name: string
@@ -12,12 +15,75 @@ export interface Props {
 
 const RepoCard = (props: Props) => {
   return (
-    <Card variant="outlined">
-      <CardContent>
-        <Avatar src={props.avtarurl} />
-      </CardContent>
-    </Card>
+    <PopupState variant="popover" popupId="demo-popup-popover">
+      {(popupState) => (
+        <Card
+          variant="outlined"
+          // {...bindHover(popupState)}
+          sx={{
+            width: '100%', height: '100%', "&:hover": {
+              backgroundColor: "#bdb3aa"
+            }
+          }}>
+          <Box display="flex" alignContent="center" alignItems="center" p={1} >
+            <Typography alignContent="center" fontFamily={lexend.style.fontFamily} flexGrow={1}>
+              {props.repo_name}
+            </Typography>
+            <StarBorderIcon /> {props.stars}
+          </Box>
+          <Divider variant="fullWidth" />
+          <Box p={2}>
+            {props.Description}
+          </Box>
+          {/* <Avatar src={props.avtarurl} /> */}
+          <HoverPopover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'center',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <Box sx={{ height: '50vh', width: '50vw' }}>
+              {props.language}
+            </Box>
+          </HoverPopover>
+        </Card>
+      )}
+    </PopupState>
   )
 }
 
 export default RepoCard;
+
+
+
+function PopoverPopupState() {
+  return (
+    <PopupState variant="popover" popupId="demo-popup-popover">
+      {(popupState) => (
+        <div>
+          <Button variant="contained" {...bindHover(popupState)}>
+            Open Popover
+          </Button>
+          <HoverPopover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+          </HoverPopover>
+        </div>
+      )}
+    </PopupState>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,23 @@ import { GitHubSearch } from '@/api/githubsearch';
 import RepoCard from '@/components/repocard';
 import { TittleBar } from '@/components/tittlaBar';
 import { Item } from '@/model/githubresmodel';
-import { Button, Container, FormControl, Grid, MenuItem, Select, TextField, Typography } from '@mui/material';
+import { Button, Container, FormControl, Grid, InputAdornment, InputLabel, MenuItem, Select, SvgIcon, TextField, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
 
 
 export default function Home() {
-  const [age, setAge] = useState(10)
+  const [stSortBy, setSortBy] = useState<"stars" | "forks" | "help-wanted-issues" | "updated" | null>(null)
+  const [stOrderBy, setOrderBy] = useState<"desc" | "asc">("asc")
   const [searchQry, setsearchQry] = useState('')
-
   const [data, setData] = useState<Item[]>([]);
 
 
   const handleSearch = async () => {
-    const responseData = await GitHubSearch.sendRequest(searchQry, 'asc', null);
+    const responseData = await GitHubSearch.sendRequest(searchQry, stOrderBy, stSortBy);
     if (responseData !== null) {
       setData(responseData?.items);
-    }
+    } else setData([])
   }
 
   return (
@@ -26,21 +26,62 @@ export default function Home() {
       <TittleBar />
       <Container>
         <Grid container justifyContent="center" alignItems="center" spacing={1}>
-          <Grid item>
-            <TextField value={searchQry} onChange={(e) => setsearchQry(e.target.value)} label="Search" variant='filled' />
+          <Grid item xl={5} >
+            <TextField
+              value={searchQry}
+              onChange={(e) => setsearchQry(e.target.value)}
+              fullWidth
+              hiddenLabel
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SvgIcon color="action" fontSize="small">
+                      <Search />
+                    </SvgIcon>
+                  </InputAdornment>
+                ),
+              }}
+              placeholder="Search Repositories"
+            />
           </Grid>
-          <Grid item>
+          <Grid item xl={3}>
             <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Sort By</InputLabel>
               <Select
-                variant='filled'
-                label="aasb"
-                value={age}
-              // label="ASdjlasbd"
-              // onChange={handleChange}
+                fullWidth
+                // variant='filled'
+                label="Sort By"
+                value={stSortBy}
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                onChange={(e) => setSortBy(e.target.value as unknown as "stars" | "forks" | "help-wanted-issues" | "updated" | null)}
+
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={'stars'}>stars</MenuItem>
+                <MenuItem value={'forks'}>forks</MenuItem>
+                <MenuItem value={'help-wanted-issues'}>help-wanted-issues</MenuItem>
+                <MenuItem value={'updated'}>updated</MenuItem>
+                <MenuItem value={null}>Remove</MenuItem>
+
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xl={3}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Order By</InputLabel>
+              <Select
+                fullWidth
+                // variant='filled'
+                label="Order By"
+                value={stOrderBy}
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                onChange={(e) => setOrderBy(e.target.value as unknown as "desc" | "asc")}
+
+              >
+                <MenuItem value={'asc'}>ascending</MenuItem>
+                <MenuItem value={'desc'}>descending</MenuItem>
+
               </Select>
             </FormControl>
           </Grid>
@@ -53,7 +94,7 @@ export default function Home() {
         <Typography variant="h6" fontWeight={700} >
           PUBLIC REPOSITORIES
         </Typography>
-        <Grid container spacing={1}>
+        <Grid container spacing={1} alignItems="stretch">
           {data.map((item) => (
             <Grid xs={12} sm={12} md={6} lg={4} xl={4} item key={item.git_url} >
               <RepoCard
@@ -71,3 +112,20 @@ export default function Home() {
     </Box >
   );
 }
+
+import { createSvgIcon } from '@mui/material/utils';
+
+export const Search = createSvgIcon(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+      clipRule="evenodd"
+    />
+  </svg>,
+  'Search'
+);
